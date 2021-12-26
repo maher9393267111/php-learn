40cba482a2fd1fad81ea9dfcde50043fe75fec98
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2021-12-26 21:26:41 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,6 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-<body>
-    <form action="" method='POST'> -->
-<!-- <div class="row mb-3">
 
-  <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Email</label> -->
+
+ send data from sql to php
   <?php
 	$dbh = mysqli_connect('localhost', 'root',  "",'mahero111');
 	//connect to MySQL server if (!$dbh)
